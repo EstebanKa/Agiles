a6b934a64d3df177c42382fea75bd97b2ef41328
estebanKa <estebankarlenaguirre@gmail.com>
2026-08-10 20:15:57 -0300

    Test verde devolucion mismo numero

--- calc.py
+++ calc.py
@@ -1,3 +1,6 @@
 def sumar(numeros):
-    return 0
-
+    if numeros == "":
+        return 0
+    else:
+        return int(numeros)
+ 
--- test.py
+++ test.py
@@ -5,3 +5,4 @@ def test_vacio_igual_0():
     
 def test_devolucion_mismo_numero():
     assert sumar("5") == 5
+
