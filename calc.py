c80d5c795d5ec05de8400e9c627fa3d21fe4803b
Mateo <mateospertino@gmail.com>
2026-08-10 20:58:06 -0300

    Test verde: logica para sumar separados por coma

--- calc.py
+++ calc.py
@@ -1,4 +1,13 @@
 def sumar(numeros):
-  return int(numeros) if numeros != "" else 0
- 
+  #Regla 1: String vacio
+  if numeros == "":
+    return 0
+
+  #Regla 2: Dos numeros separados por coma
+  if "," in numeros:
+    partes = numeros.split(",")
+    return int(partes[0]) + int(partes[1])
+
+  #Regla 3: Un solo número
+  return int(numeros)
  
