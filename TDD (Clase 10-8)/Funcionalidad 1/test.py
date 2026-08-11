4f842fa5cb83b82fe84f78499e4ade9fb579dc34
estebanKa <estebankarlenaguirre@gmail.com>
2026-08-11 09:39:22 -0300

    test

--- TDD (Clase 10-8)/Funcionalidad 1/test.py
+++ TDD (Clase 10-8)/Funcionalidad 1/test.py
@@ -6,5 +6,5 @@ def test_vacio_igual_0():
 def test_devolucion_mismo_numero():
     assert sumar("5") == 5
 
-def test_suma_dos_numeros_separados_por_coma():
+def test_debe_sumar_dos_numeros():
     assert sumar("1,2") == 3
---
+++ calc.py
@@ -0,0 +1,6 @@
+def sumar(numeros):
+    if numeros == "":
+        return 0
+    else:
+        return int(numeros)
+ 
