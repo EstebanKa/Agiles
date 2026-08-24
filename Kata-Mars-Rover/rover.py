c1f987ab6de69d9df10be64fcd2d1136a7fae936
Tomas Sanchez Machado <sanchezmachadotomas@gmail.com>
2026-08-24 20:23:13 +0000

    GREEN: Rover advances forward

--- Kata-Mars-Rover/rover.py
+++ Kata-Mars-Rover/rover.py
@@ -12,7 +12,7 @@ class Rover:
 
     def orientacion(self):
         return self.direction
-
+     
     def girar_izquierda(self):
         indice = self.direcciones_validas.index(self.direction)
         self.direction = self.direcciones_validas[(indice - 1) % 4]
@@ -20,3 +20,13 @@ class Rover:
     def girar_derecha(self):
         indice = self.direcciones_validas.index(self.direction)
         self.direction = self.direcciones_validas[(indice + 1) % 4]
+
+    def mover_adelante(self):
+        if self.direction == 'N':
+            self.y += 1
+        elif self.direction == 'S':
+            self.y -= 1
+        elif self.direction == 'E':
+            self.x += 1
+        elif self.direction == 'O':
+            self.x -= 1
